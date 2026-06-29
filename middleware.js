6e99582be63e62f05faf8a9b4b77c547b1e916ae
rootxpi <leethegreat0182@gmail.com>
2026-06-29 17:53:34 +0530

    Refactor middleware comments and response headers
    Updated comments for clarity and adjusted the unauthorized response message.

--- middleware.js
+++ middleware.js
@@ -3,23 +3,23 @@ import { NextResponse } from 'next/server';
 export function middleware(req) {
   const basicAuth = req.headers.get('authorization');
   
-  // Vercel Environment Variables me tum SITE_PASSWORD set karoge. 
-  // Agar set nahi kiya, toh default password 'Ravi@2026' rahega.
+  // Vercel me SITE_PASSWORD set karoge. Default: Ravi@2026
   const validPassword = process.env.SITE_PASSWORD || "Ravi@2026"; 
 
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1];
     const [user, pwd] = atob(authValue).split(':');
 
-    
+    // Password match check
     if (pwd === validPassword) {
       return NextResponse.next();
     }
   }
 
+
   return new NextResponse('Unauthorized Access: Contact @Ravi for Password', {
     status: 401,
-    headers: { 'WWW-Authenticate': 'Basic realm="Secure 𝖫Ξ𝖮⟁𝗘𝗬Ξ Hub"' },
+    headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
   });
 }
 
